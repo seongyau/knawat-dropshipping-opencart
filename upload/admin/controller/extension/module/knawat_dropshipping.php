38d677167abf3ffdd91f65977829f1dd3070df25
Dharmesh Patel <dspatel44@gmail.com>
2018-06-07 15:28:51 +0530

    Added Form for Consumer API Keys #1

--- upload/admin/controller/extension/module/knawat_dropshipping.php
+++ upload/admin/controller/extension/module/knawat_dropshipping.php
@@ -1,9 +1,11 @@
 <?php
 class ControllerExtensionModuleKnawatDropshipping extends Controller { 
+	
 	private $error = array();
+	private $route = 'extension/module/knawat_dropshipping';
 
 	public function index() {
-		$this->load->language('extension/module/knawat_dropshipping');
+		$this->load->language( $this->route );
 		$this->document->setTitle($this->language->get('heading_title'));
 
 		$this->load->model('setting/setting');
@@ -17,12 +19,27 @@ class ControllerExtensionModuleKnawatDropshipping extends Controller {
 			$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true));
 		}
 
+		// Load Laguage strings
+		$data = array(
+			'entry_consumer_key' 			=> $this->language->get('entry_consumer_key'),
+			'consumer_key_placeholder' 		=> $this->language->get('consumer_key_placeholder'),
+			'entry_consumer_secret' 		=> $this->language->get('entry_consumer_secret'),
+			'consumer_secret_placeholder' 	=> $this->language->get('consumer_secret_placeholder')
+		);
+
 		// Check and set warning.
 		if (isset($this->error['warning'])) {
 			$data['error_warning'] = $this->error['warning'];
 		} else {
 			$data['error_warning'] = '';
 		}
+
+		// Status Error
+		if (isset($this->error['error_status'])) {
+            $data['error_status'] = $this->error['error_status'];
+        } else {
+            $data['error_status'] = '';
+        }
 
 		// Setup Breadcrumbs Data.
 		$data['breadcrumbs'] = array();
@@ -39,28 +56,51 @@ class ControllerExtensionModuleKnawatDropshipping extends Controller {
 
 		$data['breadcrumbs'][] = array(
 			'text' => $this->language->get('heading_title'),
-			'href' => $this->url->link('extension/module/knawat_dropshipping', 'user_token=' . $this->session->data['user_token'], true)
+			'href' => $this->url->link( $this->route, 'user_token=' . $this->session->data['user_token'], true)
 		);
 
-		$data['action'] = $this->url->link('extension/module/knawat_dropshipping', 'user_token=' . $this->session->data['user_token'], true);
+		$data['action'] = $this->url->link( $this->route, 'user_token=' . $this->session->data['user_token'], true);
 
 		$data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true);
 
+		// Set module status
 		if (isset($this->request->post['module_knawat_dropshipping_status'])) {
 			$data['module_knawat_dropshipping_status'] = $this->request->post['module_knawat_dropshipping_status'];
 		} else {
 			$data['module_knawat_dropshipping_status'] = $this->config->get('module_knawat_dropshipping_status');
 		}
 
+		// Set Consumer Key
+		if (isset($this->request->post['module_knawat_dropshipping_consumer_key'])) {
+			$data['module_knawat_dropshipping_consumer_key'] = $this->request->post['module_knawat_dropshipping_consumer_key'];
+		} else {
+			$data['module_knawat_dropshipping_consumer_key'] = $this->config->get('module_knawat_dropshipping_consumer_key');
+		}
+
+		// Set Consumer Secret
+		if (isset($this->request->post['module_knawat_dropshipping_consumer_secret'])) {
+			$data['module_knawat_dropshipping_consumer_secret'] = $this->request->post['module_knawat_dropshipping_consumer_secret'];
+		} else {
+			$data['module_knawat_dropshipping_consumer_secret'] = $this->config->get('module_knawat_dropshipping_consumer_secret');
+		}
+
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
 
-		$this->response->setOutput($this->load->view('extension/module/knawat_dropshipping', $data));
+		$this->response->setOutput($this->load->view( $this->route, $data) );
 	}
 
 	protected function validate() {
-		// TODO: Form validation
+		if (!$this->user->hasPermission( 'modify', $this->route ) ) {
+            $this->error['warning'] = $this->language->get('error_permission');
+            return false;
+        }
+
+        if(!isset( $this->request->post['module_knawat_dropshipping_status'])){
+            $this->error['error_status'] = $this->language->get('error_status');
+            return false;
+        }
 		return true;
 	}
 }
--- upload/admin/language/en-gb/extension/module/knawat_dropshipping.php
+++ upload/admin/language/en-gb/extension/module/knawat_dropshipping.php
@@ -8,7 +8,12 @@ $_['text_success']     = 'Success: Knawat Dropshipping Settings has been updated
 $_['text_edit']        = 'Knawat Dropshipping Settings';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']     					= 'Status';
+$_['entry_consumer_key']     			= 'Knawat Consumer Key';
+$_['consumer_key_placeholder']     		= 'Enter Knawat Consumer Key';
+$_['entry_consumer_secret']     		= 'Knawat Consumer Secret';
+$_['consumer_secret_placeholder']     	= 'Enter Knawat Consumer Secret';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify knawat dropshipping module!';
+$_['error_permission'] 		= 'Warning: You do not have permission to modify knawat dropshipping module!';
+$_['error_status'] 	        = 'Warning: You must set status!';
